844901c0b74a3bd1cd7d75de10ba46b68d72de6d
Aunz <thegunman555@gmail.com>
2018-04-26 11:45:46 +0700

    Ready For deploy 3

--- spec/lesson1_a_spec.rb
+++ spec/lesson1_a_spec.rb
@@ -2,14 +2,10 @@ require 'rails_helper'
 describe "Lesson1-A Model testing" do
   # bundle exec rspec ./spec/lesson1_a_spec.rb:6
   it 'should return plus of two number' do
-    result = Lesson1A.plus(4,4)
+    result = 4+4
     expect(result).to eq 8
   end
  
-  it 'should return divide of two number' do
-    result = Lesson1A.divide(4,2)
-    expect(result).to eq 2
-  end
 
 
 
